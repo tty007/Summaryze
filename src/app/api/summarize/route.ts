a98f518f917ac0c0e36db4e82fffca356a353d81
tty007 <c7dc007@gmail.com>
2024-04-25 23:11:53 +0900

    add API_KEY'

--- src/app/api/summarize/route.ts
+++ src/app/api/summarize/route.ts
@@ -15,7 +15,10 @@ export default async function handler(
   if (req.method == "POST") {
     const res = await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env["OPENAI_API_KEY"]}`,
+      },
       body: JSON.stringify({
         body: req.body,
       }),
